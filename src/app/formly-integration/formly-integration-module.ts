933743a874d638fd00258cb9c1eb17c252a9626b
Abdur Rahman <abdurrahman19011@gmail.com>
2026-01-29 18:19:48 +0600

    feat: Integrate `@ngx-formly` for user form creation, including a custom repeat section field and updated application configuration.

---
+++ src/app/formly-integration/formly-integration-module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormlyUserFormRoute } from './formly-integration.routes';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FormlyUserFormRoute
+  ]
+})
+export class FormlyIntegrationModule { }
