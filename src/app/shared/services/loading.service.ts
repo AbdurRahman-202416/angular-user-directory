df41a0b9880fbe6d0167fdfa89ba57c22a3b281e
Abdur Rahman <abdurrahman19011@gmail.com>
2026-02-22 23:11:19 +0600

    feat: add loading service

---
+++ src/app/shared/services/loading.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, signal } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoadingService {
+  private activeRequests = 0;
+  isLoading = signal(false);
+
+  show() {
+    this.activeRequests++;
+    if (this.activeRequests === 1) {
+      this.isLoading.set(true);
+    }
+  }
+
+  hide() {
+    this.activeRequests--;
+    if (this.activeRequests === 0) {
+      this.isLoading.set(false);
+    }
+  }
+}
