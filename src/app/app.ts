5fe34cde62ecf24cc3f2992739a11cd2d72aeb94
Abdur Rahman <abdurrahman19011@gmail.com>
2026-02-22 23:11:19 +0600

    refactor: update app component logic

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { LoadingService } from './shared/services/loading.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatProgressBarModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('angular-user-directory');
+
+  public loadingService = inject(LoadingService);
 }
